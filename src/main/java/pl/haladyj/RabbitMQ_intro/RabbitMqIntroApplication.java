a7d352d8ea03a7eb2c8a46e1385b96db809f089c
Paweł Haładyj <pawel@haladyj.pl>
2020-05-12 20:47:35 +0200

    initial commit

---
+++ src/main/java/pl/haladyj/RabbitMQ_intro/RabbitMqIntroApplication.java
@@ -0,0 +1,13 @@
+package pl.haladyj.RabbitMQ_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/haladyj/RabbitMQ_intro/RabbitMqIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.RabbitMQ_intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
