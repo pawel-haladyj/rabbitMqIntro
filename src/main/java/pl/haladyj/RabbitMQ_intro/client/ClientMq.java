c7340b156f37e3f1f0d933061c9bee9fc8ca1430
Paweł Haładyj <pawel@haladyj.pl>
2020-05-12 21:16:41 +0200

    receiver + publisher

---
+++ src/main/java/pl/haladyj/RabbitMQ_intro/client/ClientMq.java
@@ -0,0 +1,20 @@
+package pl.haladyj.RabbitMQ_intro.client;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientMq {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/receiveMessage")
+    public String receive(){
+        Object message = rabbitTemplate.receiveAndConvert("pawel");
+        return message.toString();
+    }
+}
